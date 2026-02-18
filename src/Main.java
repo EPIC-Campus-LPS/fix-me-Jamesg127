d7a965d264dfcf900e85ef63c4b5ebeadf616cd8
Jamesg127 <jamesg127@lpsk12.org>
2026-02-18 11:25:10 -0700

    je suis un croissant

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<s_obj> array = new ArrayList<s_obj>();
+
+        // Load data
+        s_obj student1 = new s_obj("Alice", 101);
+        student1.add(85);
+        student1.add(90);
+        student1.add(78);
+        array.add(student1);
+
+        s_obj student2 = new s_obj("Bob", 102);
+        student2.add(92);
+        student2.add(88);
+        student2.add(95);
+        array.add(student2);
+
+        s_obj student3 = new s_obj("Charlie", 103);
+        student3.add(70);
+        student3.add(60);
+        student3.add(65);
+        array.add(student3);
+
+        // DOING THE MATH AND PRINTING ALL AT ONCE
+        for(int i = 0; i < array.size(); i++) {
+            s_obj temp = array.get(i);
+            double gradeNum = temp.getVal();
+            String grade = "";
+            if (gradeNum >= 90){
+                grade = "A";
+            }
+            else if (gradeNum >= 80){
+                grade = "B";
+            }
+            else if (gradeNum >= 70){
+                grade = "C";
+            }
+            else if (gradeNum >= 60){
+                grade = "D";
+            }
+            else{
+                grade = "F";
+            }
+
+            System.out.println("ID: " + temp.idNum + " | Name: " + temp.name + " | Avg: " + gradeNum + " | Grade: " + grade);
+        }
+
+        // FINDING THE BEST STUDENT
+        double best = -1;
+        String name = "";
+        for (s_obj i : array) {
+            if (i.getVal() > best) {
+                best = i.getVal();
+                name = i.name;
+            }
+        }
+        System.out.println("Top: " + name + " with " + best);
+
+        // FINDING FAILING STUDENTS
+        System.out.println("Needs Help:");
+        for(s_obj i : array) {
+            if(i.getVal() < 70) {
+                System.out.println(i.name);
+            }
+        }
+    }
+}
--- src/s_obj.java
+++ src/s_obj.java
@@ -2,27 +2,28 @@ import java.util.*;
 
 public class s_obj
 {
-    public String n;
-    public int id_num;
-    public ArrayList<Integer> l;
+
+    public String name;
+    public int idNum;
+    public ArrayList<Integer> list;
 
     public s_obj(String n, int id_num) {
-        this.n=n;
-        this.id_num = id_num;
-        l= new ArrayList<Integer>();
+        this.name = n;
+        this.idNum = id_num;
+        list= new ArrayList<Integer>();
     }
 
     public void add(int v) {
-        l.add(v);
+        list.add(v);
     }
 
     // This calculates average but returns -1 if empty
-    public double get_val() {
-        if(l.size()==0) return -1.0;
+    public double getVal() {
+        if(list.size()==0) return -1.0;
         double t = 0;
-        for(int i=0;i<l.size();i++) {
-            t+=l.get(i);
+        for(int i = 0; i < list.size(); i++) {
+            t += list.get(i);
         }
-        return t/l.size();
+        return t / list.size();
     }
 }
